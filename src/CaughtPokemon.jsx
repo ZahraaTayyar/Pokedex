2192da9b3b996bf4bebcfe4a7b9a2b0076239cc6
ZahraaTayyar <zahraatayyar@gmail.com>
2023-03-02 19:15:37 +0000

    implemented CSS

--- src/CaughtPokemon.jsx
+++ src/CaughtPokemon.jsx
@@ -6,16 +6,17 @@ const CaughtPokemon = (props) => {
     setCaught(caught.concat("Pikachu", "Eevee", "Charizard"));
   }
   return (
-    <div>
+    <div class="add-pokemon-container">
       <button onClick={catchPokemon}>
         Click to add a Pokemon to your collection!
       </button>
       <p>Caught {caught.length} Pokemon on {props.date}</p>
-      {caught.map((animal, index) => {
+      <ul>{caught.map((animal, index) => {
         return <li key={index}>{animal}</li>;
-      })}
+      })}</ul>
     </div>
   );
 };
+//ask how to make list only add one pokemon at a time to list from an array.
 
 export default CaughtPokemon;
